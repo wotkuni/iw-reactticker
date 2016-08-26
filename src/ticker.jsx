cc5c21b1525793d637283564c41b4ccd028c4595
Ian Wotkun <ian@ianwotkun.com>
2016-08-25 22:20:29 -0500

    Symbol form

--- src/ticker.jsx
+++ src/ticker.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class SymbolForm extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      symbol: ''
+    }
+  }
+
+  onSymbolChange (e) {
+    this.setState({symbol: e.target.value});
+  }
+
+  onSubmit () {
+    this.props.onFormSubmit(this.state.symbol);
+  }
+
+  render () {
+    return (
+        <form>
+          <label htmlFor="symbol">Symbol:</label>
+          <input type="text" id="symbol" />
+
+          <button type="submit">add</button>
+        </form>
+    )
+  }
+}
+
 class Ticker extends React.Component {
   constructor (props, context) {
     super(props, context);
@@ -13,22 +41,30 @@ class Ticker extends React.Component {
   getQuoteData () {
     let request = new XMLHttpRequest();
     let url = 'https://www.google.com/finance/info?client=ig&q=' + this.state.symbols.join(',');
-    let that = this;
+
+    let parseData = (request) => {
+      if (request.status >= 200 && request.status < 400) {
+        let data = request.responseText.replace(/\//g, '');
+        this.setState({quotes: JSON.parse(data)});
+      }
+    };
 
     request.open('GET', url, true);
 
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        let data = request.responseText.replace(/\//g, '');
-        that.setState({quotes: JSON.parse(data)});
-      }
-    };
+    request.onload = parseData.bind(this, request);
 
     request.onerror = function() {
       // There was a connection error of some sort
     };
 
     request.send();
+  }
+
+  addSymbol (symbol) {
+    let symbols = this.state.symbols;
+    symbols.push(symbol);
+    this.setState({symbols: symbols});
+    this.getQuoteData();
   }
 
   componentWillMount () {
@@ -42,10 +78,10 @@ class Ticker extends React.Component {
   render () {
     let renderRow = (quote, index) => {
       return (
-          <tr key={index}>
-            <td>{quote.t}</td>
-            <td>{quote.l}</td>
-          </tr>
+        <tr key={index}>
+          <td>{quote.t}</td>
+          <td>{quote.l}</td>
+        </tr>
       )
     };
 
@@ -56,6 +92,8 @@ class Ticker extends React.Component {
             { this.state.quotes.map(renderRow) }
           </tbody>
         </table>
+
+        <SymbolForm onFormSubmit={this.addSymbol} />
       </div>
     )
   }
